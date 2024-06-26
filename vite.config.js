2277bc547248636ef43703e481a129214b7c53e6
Youssef HIMMI <yhimmi1@gmail.com>
2024-06-27 00:33:09 +0200

    Fix Vite configuration and dependencies

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import postcssImport from 'postcss-import';
-import postcssNormalize from 'postcss-normalize';
-
-export default defineConfig({
-  plugins: [react(), postcssImport(), postcssNormalize()],
-});
