89544f42e1a128070e10af6875bd2e2e1674b9cb
Youssef HIMMI <yhimmi1@gmail.com>
2024-06-30 00:13:55 +0200

    adjusting another issue

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcssImport from 'postcss-import';
-import postcssUrl from 'postcss-url';
-import postcssMixins from 'postcss-mixins';
-import postcssPresetEnv from 'postcss-preset-env';
-
-// Custom PostCSS plugin to remove @charset rules
-const removeCharset = () => {
-  return {
-    postcssPlugin: 'remove-charset',
-    AtRule: {
-      charset: atRule => {
-        atRule.remove();
-      }
-    }
-  };
-};
-
-removeCharset.postcss = true;
 
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [
-        postcssImport(),
-        postcssUrl(),
-        postcssMixins(),
-        postcssPresetEnv(),
-        removeCharset()
-      ]
-    }
-  }
+    postcss: 'postcss.config.cjs',
+  },
 });
